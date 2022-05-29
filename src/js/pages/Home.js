d7748434747b5976772e775aa43f5d55a456ec9b
Jean <chakib3500@gmail.com>
2022-05-29 12:08:00 +0200

    29

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -4,38 +4,66 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
     
-    const [vitrineProducts,setVitrineProducts] = useState([]);
+    const [vitrine1Data,setvitrine1Data] = useState([]);
 
     useEffect(()=>{
-        fetch("https://raw.githubusercontent.com/J0SUKE/react-boutique-data/main/vitrine.json")
-        .then(resp=>resp.json())
-        .then(data=>setVitrineProducts(data));
+      let data = [{},{},{},{},{},{}];
+      
+      // list des produits :
+      // smartwatches 4 6 , smartphones 0 1 2 , laptops 6
+      fetch("https://raw.githubusercontent.com/J0SUKE/react-boutique-data/main/smartwatches.json")
+      .then((resp)=>resp.json())
+      .then((resp)=>{
+        data[5]
+      })
     },[])
   
-    return (
-    <div>
-        {/* <h1>Our products</h1>
-        {
-            vitrineProducts?.map(product=>(<ProductVitrine key={product.id} product={product}/>))
-        } */}
-    </div>
-
-  )
-}
-
-
-function ProductVitrine({product:{id,name,desc,price,category}}) {
-    
     return(
-      <section>
-        <h1>{name}</h1>
-        <h2>{price}</h2>
-        <p>{desc}</p>
-        <button>
-            <Link to={`/${category}/${id}`}>Acheter</Link>
-        </button>
-      </section>
-    ) 
+      <>
+        <div className="banner hero_banner">
+
+        </div>
+        <ul className="vitrine_grid vitrine_grid-1">
+          <li className='vitrine1-item'>
+            <div className="vitrine1-item__left">
+              <h1><strong>HUAWEI</strong> WATCH FIT 2</h1>
+              <p>
+                Écran HUAWEI FullView de 1,74 pouceAppels Bluetooth
+                Suivi d’un mode de vie sain
+              </p>
+              <span>à partir de</span>
+              <h2>149,99 €</h2>
+              <span>Ou payer en 4 fois</span>
+              <div className="btns">
+                <button>En savoir plus</button>
+                <button>Acheter</button>
+              </div>
+            </div>
+            <div className="vitrine1-item__right">
+              <img src="images/products/smartWatches/home/4.png" alt="" />
+            </div>
+          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+
+        <div className="banner middle-banner"></div>
+
+        <ul className="vitrine_grid vitrine-grid-2">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+
+      </>
+    )
 }
+
+
 
 export default Home
